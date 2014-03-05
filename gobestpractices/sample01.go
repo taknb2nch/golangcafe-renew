b39fe1238571035e2aef91071c5b0701da0af519
taknb2nch <taknb2nch@gmail.com>
2014-03-05 23:53:43 +0900

    すべて書き込むかすべて書き込まないか
    一旦バッファ等にためて、すべてがOKの場合にのみ書き出す。

--- gobestpractices/sample01.go
+++ gobestpractices/sample01.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
 	"io"
 	"os"
@@ -12,9 +13,9 @@ type Gopher struct {
 }
 
 type binWriter struct {
-    w    io.Writer
-    size int64
-    err  error
+    w   io.Writer
+    buf bytes.Buffer
+    err error
 }
 
 func (w *binWriter) Write(v interface{}) {
@@ -28,17 +29,25 @@ func (w *binWriter) Write(v interface{}) {
     case int:
         w.Write(int64(x))
     default:
-        if w.err = binary.Write(w.w, binary.LittleEndian, v); w.err == nil {
-            w.size += int64(binary.Size(v))
-        }
+        w.err = binary.Write(&w.buf, binary.LittleEndian, v)
     }
+}
+
+// Flush writes any pending values into the writer if no error has occurred.
+// If an error has occurred, earlier or with a write by Flush, the error is
+// returned.
+func (w *binWriter) Flush() (int64, error) {
+    if w.err != nil {
+        return 0, w.err
+    }
+    return w.buf.WriteTo(w.w)
 }
 
 func (g *Gopher) WriteTo(w io.Writer) (int64, error) {
     bw := &binWriter{w: w}
     bw.Write(g.Name)
     bw.Write(g.AgeYears)
-    return bw.size, bw.err
+    return bw.Flush()
 }
 
 func main() {
