61ba79a96f83b7a32c508870cac5278a237b04ba
taknb2nch <taknb2nch@gmail.com>
2014-03-05 23:43:17 +0900

    add some code

---
+++ gobestpractices/sample01.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/binary"
+	"io"
+	"os"
+)
+
+type Gopher struct {
+    Name     string
+    AgeYears int
+}
+
+func (g *Gopher) WriteTo(w io.Writer) (size int64, err error) {
+    err = binary.Write(w, binary.LittleEndian, int32(len(g.Name)))
+    if err == nil {
+        size += 4
+        var n int
+        n, err = w.Write([]byte(g.Name))
+        size += int64(n)
+        if err == nil {
+            err = binary.Write(w, binary.LittleEndian, int64(g.AgeYears))
+            if err == nil {
+                size += 4
+            }
+            return
+        }
+        return
+    }
+    return
+}
+
+func main() {
+	g := Gopher{Name: "Hoge", AgeYears: 5}
+	g.WriteTo(os.Stdout)
+}
